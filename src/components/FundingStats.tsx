478fe28ef8bfe1895a5106a8969abb390cd41542
magentaceiba <magenta@dorg.tech>
2024-09-09 12:16:52 -0400

    style: hide price USD as its 0

--- src/components/FundingStats.tsx
+++ src/components/FundingStats.tsx
@@ -8,10 +8,10 @@ export function FundingStats() {
   const { symbol, totalSupply } = useFundingStats()
   return (
     <Stats className="bg-base-200 scale-75">
-      <Stats.Stat>
+      {/* <Stats.Stat>
         <Stats.Stat.Item variant="title">Price Usd</Stats.Stat.Item>
         <Stats.Stat.Item variant="value">{'1.00 $'}</Stats.Stat.Item>
-      </Stats.Stat>
+      </Stats.Stat> */}
 
       <Stats.Stat>
         <Stats.Stat.Item variant="title">{symbol} TVL</Stats.Stat.Item>
